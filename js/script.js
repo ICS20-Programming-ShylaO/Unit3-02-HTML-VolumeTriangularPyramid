406c78172e9a6e0df607b6109447ec67c70e24dd
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-14 14:30:51 +0000

    updated comments in js

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@
  * This function calculates the volume of a right triangular pyramid.
  */
 function calculateVolume () {
-  // input
+  // get side a, b, and height from user
   let sideA = parseFloat(document.getElementById('side-a-tri-pyramid').value);
   let sideB = parseFloat(document.getElementById('side-b-tri-pyramid').value);
   let height = parseFloat(document.getElementById('height-tri-pyramid').value);
 
-  // process
+  // calculate volume
   let volume = (1/6 * sideA * sideB * height);
 
-  // output
+  // display volume to user
   document.getElementById('volume').innerHTML = "Volume is: " + volume.toFixed(2) + " cm<sup>3</sup>."
 }
